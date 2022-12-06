d261e5759ab179ea0202ef98079cbfa4eaea172b
Chiriko <sofia.digennarox@gmail.com>
2022-12-06 12:50:44 +0100

    Creating the project
    to-do list

---
+++ Angular_exercise/todo_app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TodoComponent } from './todo/todo.component';
+import { AddTodoComponent } from './todo/add-todo/add-todo.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoItemComponent } from './todo/todo-item/todo-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    TodoComponent,
+    AddTodoComponent,
+    TodoListComponent,
+    TodoItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
