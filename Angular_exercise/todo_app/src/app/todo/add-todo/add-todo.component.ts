74faccffbc7f0ea5d8b35cd0f95f37a7d7ed1ca4
Chiriko <sofia.digennarox@gmail.com>
2022-12-06 19:17:15 +0100

    Working on the structure of the project
    to-do list

--- Angular_exercise/todo_app/src/app/todo/add-todo/add-todo.component.ts
+++ Angular_exercise/todo_app/src/app/todo/add-todo/add-todo.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-todo',
   templateUrl: './add-todo.component.html',
   styleUrls: ['./add-todo.component.css']
 })
-export class AddTodoComponent {
-
+export class AddTodoComponent implements OnInit{
+  @Input() taskName: string = '';
+  
+  ngOnInit(): void {
+    
+  }
+  addTask(){
+    console.log("New task")
+  }
 }
