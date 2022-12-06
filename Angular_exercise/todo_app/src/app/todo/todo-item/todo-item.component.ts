e5e500d47dcd55b1a41fdd65463eca0167e1e30c
Chiriko <sofia.digennarox@gmail.com>
2022-12-06 19:17:15 +0100

    Working on the structure of the project
    to-do list app

--- Angular_exercise/todo_app/src/app/todo/todo-item/todo-item.component.ts
+++ Angular_exercise/todo_app/src/app/todo/todo-item/todo-item.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.css']
 })
-export class TodoItemComponent {
-
+export class TodoItemComponent implements OnInit{
+  task: string[] = [];
+    
+  constructor() {}
+    
+  ngOnInit() {
+      this.task = ['Veni','Vidi','Vici'];
+  }
 }
